c61c23538529b4bb9c5017137fabc97652a0fdd2
unknown <edwinngera@gmail.com>
2022-12-21 10:04:23 +0300

    handle profile signup

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleSignUp() {
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(
     (results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
