6ade840998dd9a7b780cbac4979f5fcc824f8451
unknown <edwinngera@gmail.com>
2023-01-03 11:44:07 +0300

    fix eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,8 @@
-export default function getListStudents()
-{
-    students=[
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-      ]
-
-      return students
+export default function getListStudents() {
+  var students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
 }
